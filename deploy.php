ca588e5abaa5ef21d680e453293684658268ffba
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2024-03-16 19:45:20 +0100

    deploy using deployer

---
+++ deploy.php
@@ -0,0 +1,28 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+// Project name
+set('application', 'Laravel-auth');
+
+// Project repo
+set('repository', 'https://github.com/Ayenko4all/lara-basic-auth-api.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+host('18.188.54.27')
+    ->setRemoteUser('ubuntu')
+    ->set('branch', 'deploy-to-aws-ec2')
+    ->set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader')
+    ->set('deploy_path', '~/var/www/current');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
