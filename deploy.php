aacb611916fdf7a768aeffc591dce494aec23fb2
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2024-03-16 20:53:33 +0100

    update

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@ set('writable_chmod_mode', '0755');
 host('18.188.54.27')
     ->setRemoteUser('ubuntu')
     ->set('branch', 'deploy-to-aws-ec2')
-    ->set('composer_options', 'composer install --optimize-autoloader --no-dev')
+    //->set('composer_options', 'composer install --optimize-autoloader --no-dev')
     ->set('deploy_path', '~/var/www/current');
 
 // Hooks
