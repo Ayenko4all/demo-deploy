2d236bead1eebcfc161c9b2b861b2b666c575a6e
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2024-03-16 20:56:51 +0100

    updates

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::prefix('api')->group(function () {
     Route::get('/', function () {
         return response()->json([
             'status' => 'success',
-            'data' => ['message' => 'Welcome to the Auth API'],
+            'data' => ['message' => 'Welcome to the Laravel Auth API'],
         ]);
     })->name('index');
 
